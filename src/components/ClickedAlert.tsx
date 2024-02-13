4ca8cb0a5fbfd9f6b1ee73041734594aa554531e
VannWasHere <ateen1203@gmail.com>
2024-02-13 11:29:54 +0700

    Adding Update

--- src/components/ClickedAlert.tsx
+++ src/components/ClickedAlert.tsx
@@ -1,17 +1,31 @@
-import { ReactElement, createElement } from "react";
-export interface AlertProps {
-  alertMessage: string;
-}
-
-export default function ClickedAlert({ alertMessage }: AlertProps): ReactElement {
-
-  const showsAlert = () => {
-        setTimeout(() => {
-        console.log(alertMessage);
+import { ReactElement, createElement, useState, useEffect } from "react";
+import Alert from "./Alert";
+export interface ClickedAlertProps {
+    alertMessage: string;
+  }
+  
+  export default function ClickedAlert({ alertMessage }: ClickedAlertProps): ReactElement {
+    const [showNotif, setShowNotif] = useState<boolean>(false);
+  
+    useEffect(() => {
+      if (showNotif) {
+        const timeoutId = setTimeout(() => {
+          setShowNotif(false);
         }, 2000);
+  
+        // Cleanup the timeout if the component unmounts before the 2 seconds are up
+        return () => clearTimeout(timeoutId);
+      }
+    }, [showNotif]);
+  
+    const handleClick = () => {
+      setShowNotif(true);
     };
-
-  return (
-    <button onClick={showsAlert}>Click Me</button>
-  );
-}
+  
+    return (
+      <div>
+        <button onClick={handleClick}>Click Me!</button>
+        <div>{showNotif && <Alert alertMessage={alertMessage} />}</div>
+      </div>
+    );
+  }
